feee2760a127fb859c55b00c57b94b3611e11987
zietraore <2610zitraore@student.epfc.eu>
2026-02-09 15:58:10 +0100

    feat(feat_model_actions): Creation des premiers classes

---
+++ src/main/java/scratch/model/Action.java
@@ -0,0 +1,11 @@
+package scratch.model;
+
+public abstract class  Action {
+  public void execute(ExecutionContext e ){
+
+  }
+
+   public boolean isValid(ExecutionContext e ){
+      return false;
+   }
+}
