c0647a3522a9a5acba90d7cd3202adcc8c013f3f
Sam Prophete NSENGIMANA <0609sansengimana@student.epfc.eu>
2026-02-09 16:27:42 +0100

    (model)execute context commencement

--- src/main/java/scratch/model/ExecutionContext.java
+++ src/main/java/scratch/model/ExecutionContext.java
@@ -1,4 +1,53 @@
 package scratch.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExecutionContext {
+    private int x ;
+    private int y ;
+    private int direction ;
+    private boolean penDown ;
+    private List<Segment> segmentList = new ArrayList<>();
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public boolean isPenDown() {
+        return penDown;
+    }
+
+    public List<Segment> getSegmentList() {
+        return segmentList;
+    }
+    public int getSegmentsCount(List<Segment> segments){
+        return segments.size();
+    }
+    public void move(int indx){
+
+    }
+    public void turnLeft(int indx){
+
+    }
+    public void turnRight(int indx){
+
+    }
+    public void penUp(int indx){
+
+    }
+    public void penDown(int indx){
+
+    }
+    public void reset(){
+        
+    }
 }
--- src/main/java/scratch/model/Segment.java
+++ src/main/java/scratch/model/Segment.java
@@ -1,4 +1,24 @@
 package scratch.model;
 
 public class Segment {
+    private int x1 ;
+    private int y1 ;
+    private int x2 ;
+    private int y3 ;
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY3() {
+        return y3;
+    }
 }
