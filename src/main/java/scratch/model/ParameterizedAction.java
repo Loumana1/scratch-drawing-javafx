31d4821cc773a9b07f4f80a55b1bef4b8e51912b
Hugo CASTELAIN <2006hucastelain@student.epfc.eu>
2026-02-09 17:00:49 +0100

    model parameterizedAction

--- src/main/java/scratch/model/ParameterizedAction.java
+++ src/main/java/scratch/model/ParameterizedAction.java
@@ -1,7 +1,18 @@
 package scratch.model;
 
 public abstract class  ParameterizedAction {
+
     private int value ;
 
+    public ParameterizedAction(int value) {
+        this.value = value;
+    }
 
+    public int getValue(){
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
