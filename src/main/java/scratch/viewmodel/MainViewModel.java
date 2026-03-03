d0591e877decebd1a5aaf4ae6559a20b6ed89b11
sam_prophete <0609sansengimana@student.epfc.eu>
2026-03-03 19:29:31 +0100

    (debug_merge)

--- src/main/java/scratch/viewmodel/MainViewModel.java
+++ src/main/java/scratch/viewmodel/MainViewModel.java
@@ -80,6 +80,26 @@ public class MainViewModel {
         this.observableActions.add(action);
         this.selectedIndex.set(this.observableActions.size() - 1);
     }
+    //Turn Right
+    public void addTurnRight() {
+        TurnRightAction action = new TurnRightAction(90);
+        this.program.addAction(action);
+        this.observableActions.add(action);
+        this.selectedIndex.set(this.observableActions.size() - 1);
+    }
+
+    //Move Up
+    public void moveUp() {
+        program.moveUp(selectedIndex.get());
+        this.selectedIndex.set(selectedIndex.get() - 1);
+    }
+
+    //Move Down
+    public void moveDown() {
+        program.moveDown(selectedIndex.get());
+        this.selectedIndex.set(selectedIndex.get() + 1);
+    }
+
 
     //Boutton Suprrimer une action
     public void removeSelectedAction() {
