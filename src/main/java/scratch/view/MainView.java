45cd2736b53f9be9a1b15147dd6cf359cd426730
sam_prophete <0609sansengimana@student.epfc.eu>
2026-03-08 14:45:31 +0100

    (Progrmme view)debug monter descendre

--- src/main/java/scratch/view/MainView.java
+++ src/main/java/scratch/view/MainView.java
@@ -1,50 +1,25 @@
 package scratch.view;
 
-import javafx.scene.control.ListView;
 import javafx.scene.layout.*;
-import scratch.model.Action;
 import scratch.viewmodel.MainViewModel;
 
 import javafx.geometry.Insets;
 
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-
 
 public class MainView extends BorderPane {
 
     private final MainViewModel viewModel;
 
-    private final ListView<Action> programListView = new ListView<>();
-
     public MainView(MainViewModel viewModel) {
         this.viewModel = viewModel;
-    //Gauche
+
         PaletteView palette = new PaletteView(viewModel);
         ProgramView program = new ProgramView(viewModel);
         SceneView scene = new SceneView(viewModel);
 
-        //
-
-
-        //
         HBox center = new HBox(20);
         center.getChildren().addAll(program, scene);
-
-        //detailview
-        programListView.setItems(viewModel.getObservableActions());
-
-
-        // Synchro sélection ListView -ViewModel
-        programListView.getSelectionModel().selectedIndexProperty()
-                .addListener((obs, old, nw) ->
-                        viewModel.selectedIndexProperty().setValue(nw.intValue()));
-        viewModel.selectedIndexProperty()
-                .addListener((obs, old, nw) ->
-                        programListView.getSelectionModel().select(nw.intValue()));
-
-
-
 
         setLeft(palette);
         setCenter(center);
